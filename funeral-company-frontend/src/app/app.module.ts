ab60b1f55c69e0d5c34f5c7484c0eae1b3125764
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-07-03 19:42:16 +0200

    Added angular material, bootstrap, user components

--- funeral-company-frontend/src/app/app.module.ts
+++ funeral-company-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { RegisterUserComponent } from './register-user/register-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
