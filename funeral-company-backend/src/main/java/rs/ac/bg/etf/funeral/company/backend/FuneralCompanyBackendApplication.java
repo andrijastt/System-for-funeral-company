277b7f64cfcfb2d2f83b6d77bb9c97955235ef78
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-07-03 18:30:30 +0200

    Added backend and frontend

---
+++ funeral-company-backend/src/main/java/rs/ac/bg/etf/funeral/company/backend/FuneralCompanyBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.funeral.company.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralCompanyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneralCompanyBackendApplication.class, args);
+	}
+
+}
---
+++ funeral-company-backend/src/test/java/rs/ac/bg/etf/funeral/company/backend/FuneralCompanyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.etf.funeral.company.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuneralCompanyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
