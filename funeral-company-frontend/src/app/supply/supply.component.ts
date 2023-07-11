6038f21cce2c332589c23c963104dfad2f03c4a4
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-07-11 16:49:49 +0200

    Select option working

--- funeral-company-frontend/src/app/supply/supply.component.ts
+++ funeral-company-frontend/src/app/supply/supply.component.ts
@@ -4,6 +4,7 @@ import { Material } from '../models/Material';
 import { Supply } from '../models/Supply';
 import { MaterialService } from '../material.service';
 import { MaterialSupply } from '../models/MaterialSupply';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-supply',
@@ -16,6 +17,7 @@ export class SupplyComponent {
   
   supplies: Supply[]
   materialSelect: Material[]  
+  materialSelected: number[] = []
 
   ngOnInit(){
     this.supplyService.getAllSupplies().subscribe((data: Supply[])=>{
@@ -32,8 +34,9 @@ export class SupplyComponent {
 
   materialSupply: MaterialSupply[] = []
 
-  addMoreMaterial(){            
-    this.materialSupply.push(new MaterialSupply())
+  addMoreMaterial(){                    
+    this.materialSupply.push(new MaterialSupply())    
+    this.materialSelected.push(0)
   }
 
   removeMaterial(i: number){   
@@ -41,7 +44,24 @@ export class SupplyComponent {
       alert("Can't remove material, needs to be one")
     } else {      
       this.materialSupply.splice(i, 1)      
+      this.materialSelected.splice(i, 1)      
     }
+  }
+
+  onChangeMaterial(e: MatSelectChange, i: number){
+    
+    let temp: number = this.materialSelected.indexOf(e.value)
+    console.log(temp)
+    console.log(this.materialSelected)
+
+    if(temp == -1){
+      this.materialSelected[i] = e.value
+    } else {
+      alert("Material already selected")            
+      this.materialSupply[i].materialID = null
+      this.materialSelected[i] = 0
+    }
+    console.log(this.materialSelected)
   }
 
   saveSupply(){
@@ -52,7 +72,7 @@ export class SupplyComponent {
       this.dataNotFilled = true
     }
 
-    for(let material of this.materialSupply){
+    for(let material of this.materialSupply){      
       if(material.materialID == null || material.price == null || material.amount == null){
         this.dataNotFilled = true
         break
