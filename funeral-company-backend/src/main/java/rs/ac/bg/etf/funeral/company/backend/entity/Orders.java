b1a402a2f0cd6c7f20edf9fc13473a9e87b76921
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-07-14 18:10:36 +0200

    Entity orders and items

---
+++ funeral-company-backend/src/main/java/rs/ac/bg/etf/funeral/company/backend/entity/Orders.java
@@ -0,0 +1,38 @@
+package rs.ac.bg.etf.funeral.company.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderID;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(
+        cascade = CascadeType.REMOVE,
+        fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            name = "contractID",
+            referencedColumnName = "contractID"
+    )
+    private Contract contract;
+
+    @Column(nullable = false)
+    private Date dateSend;
+
+    @Column(nullable = false)
+    private Date dateArrived;
+}
