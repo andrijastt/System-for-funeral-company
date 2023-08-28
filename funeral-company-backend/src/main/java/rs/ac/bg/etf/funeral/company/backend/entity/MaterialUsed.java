30a36832249e152269635181eb92bf914cc3754a
Andrija Stojanović <stojanovicandrija123@gmail.com>
2023-08-28 22:23:30 +0200

    Small fixes

--- funeral-company-backend/src/main/java/rs/ac/bg/etf/funeral/company/backend/entity/MaterialUsed.java
+++ funeral-company-backend/src/main/java/rs/ac/bg/etf/funeral/company/backend/entity/MaterialUsed.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.funeral.company.backend.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,6 @@ public class MaterialUsed {
     private MaterialUsedPK materialUsedPK;
 
     @Column(nullable = false)
+    @Min(value = 1)
     private int amount;
 }
